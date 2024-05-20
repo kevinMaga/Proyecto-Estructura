29e5da538d6e705ed43cd79d0dafd7a902c962a4
kevinMaga <kerimaga@espol.edu.ec>
2024-05-19 21:36:38 -0500

    Concesionaria

--- Concesionaria/src/main/java/com/mycompany/concesionaria/Concesionaria.java
+++ Concesionaria/src/main/java/com/mycompany/concesionaria/Concesionaria.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.proyecto.estructura;
+package com.mycompany.concesionaria;
 
 /**
  *
  * @author Kevin
  */
-public class ProyectoEstructura {
+public class Concesionaria {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
