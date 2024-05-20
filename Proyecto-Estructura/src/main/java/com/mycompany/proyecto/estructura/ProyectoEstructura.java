996fa5ddf4bf9c4985ee1dbd71bc17301d6f03cc
kevinMaga <kerimaga@espol.edu.ec>
2024-05-19 21:31:41 -0500

    CreacionDelProyecto

---
+++ Proyecto-Estructura/src/main/java/com/mycompany/proyecto/estructura/ProyectoEstructura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto.estructura;
+
+/**
+ *
+ * @author Kevin
+ */
+public class ProyectoEstructura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
