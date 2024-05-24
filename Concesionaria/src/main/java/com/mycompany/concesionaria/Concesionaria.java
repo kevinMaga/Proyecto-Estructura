626f3339b0bd85a80ed01fc44f5f93ca3fd8777f
kevinMaga <kerimaga@espol.edu.ec>
2024-05-24 10:04:41 -0500

    Cambio

--- Concesionaria/src/main/java/com/mycompany/concesionaria/Concesionaria.java
+++ Concesionaria/src/main/java/com/mycompany/concesionaria/Concesionaria.java
@@ -11,6 +11,6 @@ package com.mycompany.concesionaria;
 public class Concesionaria {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello");
     }
 }
