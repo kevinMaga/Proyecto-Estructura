a19cfc9b1fdb5be4700e1b5880ca2eb55c4b6a5a
kevinMaga <kerimaga@espol.edu.ec>
2024-05-25 07:26:39 -0500

    autosUsadosControlador

--- Concesionaria/src/main/java/com/espol/concesionaria/PaginaAutosUsadosController.java
+++ Concesionaria/src/main/java/com/espol/concesionaria/PaginaAutosUsadosController.java
@@ -6,7 +6,13 @@ package com.espol.concesionaria;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 /**
  * FXML Controller class
@@ -14,6 +20,23 @@ import javafx.fxml.Initializable;
  * @author Kevin
  */
 public class PaginaAutosUsadosController implements Initializable {
+
+    @FXML
+    private HBox head;
+    @FXML
+    private ComboBox<?> cmbHead1;
+    @FXML
+    private TextField TFHead1;
+    @FXML
+    private ImageView IVBuscar1;
+    @FXML
+    private Label LBIniciarSesion1;
+    @FXML
+    private HBox menu;
+    @FXML
+    private ImageView IVInicio;
+    @FXML
+    private HBox filtro;
 
     /**
      * Initializes the controller class.
