bd1a5b2d5fa1cabee9549232dcf39fe8de5b0da5
kevinMaga <kerimaga@espol.edu.ec>
2024-05-24 19:23:40 -0500

    creacionAutosUsados

--- Concesionaria/src/main/java/module-info.java
+++ Concesionaria/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.espol.concesionaria {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires javafx.base;
+    requires javafx.graphics;
 
     opens com.espol.concesionaria to javafx.fxml;
     exports com.espol.concesionaria;
