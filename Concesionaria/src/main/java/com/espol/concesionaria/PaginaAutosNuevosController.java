d185c68e05bc18ed2420259cea5eff867aa36eb2
elwese <cesar.ews@gmail.com>
2024-05-26 18:12:28 -0500

    Avance en carros nuevos

--- Concesionaria/src/main/java/com/espol/concesionaria/PaginaAutosNuevosController.java
+++ Concesionaria/src/main/java/com/espol/concesionaria/PaginaAutosNuevosController.java
@@ -8,7 +8,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -25,6 +28,69 @@ public class PaginaAutosNuevosController implements Initializable {
      */
     @FXML
     private ImageView IVInicio;
+    @FXML
+    private ImageView icTAuto;
+    @FXML
+    private ImageView icTMoto;
+    @FXML
+    private ImageView icTPesado;
+    @FXML
+    private ImageView icTMaq;
+    @FXML
+    private ImageView icTAcuat;
+    @FXML
+    private ImageView icMAudi;
+    @FXML
+    private ImageView icMbmw;
+    @FXML
+    private ImageView icMbobcat;
+    @FXML
+    private ImageView icMcat;
+    
+    
+    
+    
+    public static Stage irPantallaMarca(double n,double m,String Nombre) throws IOException {
+        Stage nuevaVentana = new Stage();
+        
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("paginaPorMarca.fxml"));
+        Parent root = loader.load();
+        PaginaPorMarcaController controller = loader.getController();
+        controller.setDatos(Nombre);
+        
+        
+        
+        Scene nuevaScene = new Scene(root, n, m);
+        
+        
+        
+        nuevaVentana.setScene(nuevaScene);
+        nuevaVentana.setResizable(false);
+        nuevaVentana.show();
+        return nuevaVentana;
+    }
+    public static Stage irPantallaTipo(double n,double m,String Nombre) throws IOException {
+        Stage nuevaVentana = new Stage();
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("paginaPorTipo.fxml"));
+        Parent root = loader.load();
+        //PaginaPorTipoController controller = loader.getController();
+        //controller.setDatos(Nombre);
+        
+        
+        Scene nuevaScene = new Scene(root, n, m);
+        
+        
+        
+        nuevaVentana.setScene(nuevaScene);
+        nuevaVentana.setResizable(false);
+        nuevaVentana.show();
+        return nuevaVentana;
+    }
+    
+    
+    
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -37,6 +103,20 @@ public class PaginaAutosNuevosController implements Initializable {
                 ex.printStackTrace();
             }
         });
+        icTAuto.setOnMouseClicked(e->{
+            Stage ventanaActual = (Stage) icTAuto.getScene().getWindow();
+            ventanaActual.close();
+            try {
+                irPantallaTipo(929, 681,"Auto");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        
+        
+        
+        
+        
     }    
     
 }
---
+++ Concesionaria/src/main/java/com/espol/concesionaria/PaginaPorMarcaController.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.espol.concesionaria;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Cesar
+ */
+public class PaginaPorMarcaController implements Initializable {
+    @FXML
+    private ImageView IVInicio;
+    @FXML
+    private Label volver;
+    
+    public void setDatos(String tipo){
+    
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        IVInicio.setOnMouseClicked(e->{
+            Stage ventanaActual = (Stage) IVInicio.getScene().getWindow();
+            ventanaActual.close();
+            try {
+                App.abrirNuevaVentana("paginaPrincipal", 929, 681);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        volver.setOnMouseClicked(e->{
+            Stage ventanaActual = (Stage) IVInicio.getScene().getWindow();
+            ventanaActual.close();
+            try {
+                App.abrirNuevaVentana("paginaAutosNuevos", 929, 681);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }    
+    
+}
